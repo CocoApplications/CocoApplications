7197f3c5dd94da0223da897c16dcf217f96e9cde
rohankotwani <rohankotwani@gmail.com>
2017-09-10 14:15:32 -0400

    update time series library

--- ipython_to_markdown.py
+++ ipython_to_markdown.py
@@ -4,6 +4,6 @@ script = open(sys.argv[1]).read()
 
 script = script.replace(".png)",".png' /></p>").replace("![png]","<p><img src='").replace("```python","{% highlight python %}").replace("```","{% endhighlight %}")
 
-script = script.replace("(output_","/amazon_images/output_")
+script = script.replace("(output_","/signal_images/output_")
 
 print(script)
